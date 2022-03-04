bbe617b67accc25f722481a72e65dea3eb1b9f53
AlvaroQA <jersonaqa@gmail.com>
2022-03-03 20:45:15 -0500

    Prueba8.2

--- maintenance_equipment_soltecn/models/maintenance.py
+++ maintenance_equipment_soltecn/models/maintenance.py
@@ -10,11 +10,11 @@ class MaintenanceEquipment(models.Model):
 
     @api.model
     def send_email_custom(self):
-        for number in range(1):
+        for record in self:
             template_id = self.env['mail.template'].search([('id', '=', 13)], limit=1).id
             # for maintenance in self:
             # self.env['mail.template'].browse(template_id).send_mail(self.id, force_send=True)
-            # raise ValidationError("INGRESA")
+            raise ValidationError("INGRESA")
             last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
             start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
             maintenance_equipment_to_report = self.env["maintenance.equipment"].search([('__last_update', '>=', start_day_of_prev_month),('__last_update', '<=', last_day_of_prev_month)])
