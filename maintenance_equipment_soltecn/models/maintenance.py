20a022811073a109881667df6c1b2b1bdefa3d12
AlvaroQA <jersonaqa@gmail.com>
2022-03-04 11:04:15 -0500

    Prueba9.7

--- maintenance_equipment_soltecn/models/maintenance.py
+++ maintenance_equipment_soltecn/models/maintenance.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models
 from datetime import date, timedelta
 from odoo.exceptions import UserError, ValidationError
+import base64
 
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
@@ -18,7 +19,7 @@ class MaintenanceEquipment(models.Model):
         maintenance_equipment_to_report = self.env["maintenance.equipment"].search([('__last_update', '>=', start_day_of_prev_month),('__last_update', '<=', last_day_of_prev_month)])
 
         # data, data_format = self.env.ref('studio_customization.studio_report_docume_8f3425e2-e80d-4aca-8c43-e8d80dfbe347').sudo()._render_qweb_pdf(maintenance_equipment_to_report.ids)
-        data, data_format = self.env.ref('studio_customization.equipo_de_mantenimie_b2fb437d-6e04-4ef9-a3b8-242087bd633f').sudo()._render_qweb_pdf([1578])
+        data = self.env.ref('studio_customization.equipo_de_mantenimie_b2fb437d-6e04-4ef9-a3b8-242087bd633f').sudo()._render_qweb_pdf([1578])
         
         # result = base64.b64encode(result)
 
@@ -68,17 +69,17 @@ class MaintenanceEquipment(models.Model):
         # content = self.env.ref('studio_customization.studio_report_docume_8f3425e2-e80d-4aca-8c43-e8d80dfbe347')._render_qweb_pdf(maintenance_equipment_to_report.ids)[0]
         # content = self.env.ref('studio_customization.studio_report_docume_8f3425e2-e80d-4aca-8c43-e8d80dfbe347')._render(report_values)
 
-        # data_id = self.env['ir.attachment'].create({
-        #     'name': _("Reporte de activos (%s - %s).pdf", (str(last_day_of_prev_month), str(start_day_of_prev_month))),
-        #     'type': 'binary',
-        #     'datas': base64.encodebytes(content),
-        #     'res_model': self._name,
-        #     'res_id': self.id
-        # })
+        data_id = self.env['ir.attachment'].create({
+            'name': _("Reporte de activos (%s - %s).pdf", (str(last_day_of_prev_month), str(start_day_of_prev_month))),
+            'type': 'binary',
+            'datas': base64.encodebytes(data),
+            'res_model': self._name,
+            'res_id': self.id
+        })
 
-        # template_id.attachment_ids = [(6, 0, [data_id.id])]
+        template_id.attachment_ids = [(6, 0, [data_id.id])]
         self.env['mail.template'].browse(template_id).send_mail(self.id, force_send=True)
         # email_values = {'email_to': self.partner_id.email,
         #                 'email_from': self.env.user.email}
         # template_id.send_mail(self.id, email_values=email_values, force_send=True)
-        # template_id.attachment_ids = [(3, data_id.id)]
+        template_id.attachment_ids = [(3, data_id.id)]
