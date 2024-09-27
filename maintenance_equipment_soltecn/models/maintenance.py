ae69ed0f34c9e257ea306b6268c33c65a7627b3d
oxxo2022 <ivan.moyano@oxxo.com>
2024-09-27 16:35:41 -0500

    probando commit

--- maintenance_equipment_soltecn/models/maintenance.py
+++ maintenance_equipment_soltecn/models/maintenance.py
@@ -10,7 +10,6 @@ from odoo.tools.misc import xlsxwriter
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
-    #test
     x_studio_ubicacion_activo_name = fields.Char(related="x_studio_ubicacin_activo.x_name", string="Estado nombre")
 
     def _enviar_reporte_activos(self):
