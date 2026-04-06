9b846ef12ee6d1ad5185b3d31e24000b86352182
oxxo2022 <ivan.moyano@oxxo.com>
2026-04-06 17:30:22 -0500

    TEST odoo

--- maintenance_equipment_soltecn/models/maintenance.py
+++ maintenance_equipment_soltecn/models/maintenance.py
@@ -345,13 +345,14 @@ class MaintenanceEquipment(models.Model):
         #s
         headers = [
             "Nombre del equipo",
-            "Marca",
-            "Modelo",
+            "Marca / Modelo",
             "N° de serie",
             "Estado",
-            "Fecha de ultima ubicación",
-            "Ultima ubicación ",
-            "Ubicación actual",
+            "Fecha Últ. Movimiento",
+            "Ultima ubicación",
+            "Detalle Últ. Ubicación",
+            "Ubicación Actual",
+            "Detalle Ubicación Actual",
         ]
 
         rows = []
@@ -360,19 +361,34 @@ class MaintenanceEquipment(models.Model):
             line_last_loc_in_shop = False
             line_last_loc_in_shop_date = False
 
-            messages = self.env['mail.message'].search([
+            messages_detail = self.env['mail.message'].search([
                 ('model', '=', line._name),
                 ('res_id', '=', line.id),
                 ('tracking_value_ids.field_id.name', '=', 'x_studio_detalle_ubicacin_activo'),
             ], order='create_date desc')
 
+            messages = self.env['mail.message'].search([
+                ('model', '=', line._name),
+                ('res_id', '=', line.id),
+                ('tracking_value_ids.field_id.name', '=', 'x_studio_ubicacin_activo'),
+            ], order='create_date desc')
+
             # Iteramos buscando el último cambio DESDE una ubicación llamada "Tienda"
             #tienda_location = self.env['x_detalleubicacionacti'].search([('x_name', 'ilike', 'TIENDA%')], limit=1)
-            tienda_location = self.env['x_detalleubicacionacti'].search([])
+            
+
+            #CAMPOS DETALLE DE UBICACION
             line_last_loc_in_shop = False
             line_last_loc_in_shop_date = False
+            tienda_location = self.env['x_detalleubicacionacti'].search([])
+        
+            #CAMPOS DE UBICACION
+            line_last_main_loc_in_shop = False
+            line_last_main_loc_in_shop_date = False
+            tienda_main_location = self.env['x_ubicacionactivo'].search([])
 
-            for msg in messages:
+            #DETALLE DE UBICACION
+            for msg in messages_detail:
                 for track in msg.tracking_value_ids:
                     if track.field_id.name == 'x_studio_detalle_ubicacin_activo' and int(track.old_value_integer or 0) in tienda_location.ids:
                         line_last_loc_in_shop = tienda_location.filtered(lambda x: x.id == int(track.old_value_integer))
@@ -382,7 +398,7 @@ class MaintenanceEquipment(models.Model):
                     break
 
             if not line_last_loc_in_shop:
-                for msg in messages:
+                for msg in messages_detail:
                     for track in msg.tracking_value_ids:
                         if track.field_id.name == 'x_studio_detalle_ubicacin_activo' and int(track.new_value_integer or 0) in tienda_location.ids:
                             if track.new_value_integer != line.x_studio_detalle_ubicacin_activo.id:
@@ -392,15 +408,37 @@ class MaintenanceEquipment(models.Model):
                     if line_last_loc_in_shop_date:
                         break
             
+            #UBICACION
+            for msg in messages:
+                for track in msg.tracking_value_ids:
+                    if track.field_id.name == 'x_studio_ubicacin_activo' and int(track.old_value_integer or 0) in tienda_main_location.ids:
+                        line_last_main_loc_in_shop = tienda_main_location.filtered(lambda x: x.id == int(track.old_value_integer))
+                        line_last_main_loc_in_shop_date = msg.create_date.strftime('%d/%m/%Y')
+                        break
+                if line_last_main_loc_in_shop_date:
+                    break
+
+            if not line_last_main_loc_in_shop:
+                for msg in messages:
+                    for track in msg.tracking_value_ids:
+                        if track.field_id.name == 'x_studio_ubicacin_activo' and int(track.new_value_integer or 0) in tienda_main_location.ids:
+                            if track.new_value_integer != line.x_studio_detalle_ubicacin_activo.id:
+                                line_last_main_loc_in_shop = tienda_main_location.filtered(lambda x: x.id == int(track.new_value_integer))
+                                line_last_main_loc_in_shop_date = msg.create_date.strftime('%d/%m/%Y')
+                                break
+                    if line_last_loc_in_shop_date:
+                        break
+            
             #raise UserError(str(line_name))
             rows.append((
                 line_name,
-                line.x_studio_marca,
-                line.model,
+                "%s / %s" % (line.x_studio_marca, line.model) if line.model else line.x_studio_marca,
                 line.serial_no,
                 line.x_studio_estado,
                 line_last_loc_in_shop_date or 'NO',
+                line_last_main_loc_in_shop.x_name if line_last_main_loc_in_shop else 'NO',
                 line_last_loc_in_shop.x_name if line_last_loc_in_shop else 'NO',
+                line.x_studio_ubicacin_activo.x_name or 'NO',
                 line.x_studio_detalle_ubicacin_activo.x_name or 'NO',
             ))
 
